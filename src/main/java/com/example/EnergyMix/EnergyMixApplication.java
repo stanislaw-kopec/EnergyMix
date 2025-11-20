38e1f4c3ffccd57ef495bc6680083a1b82d7960b
stanislaw-kopec <stachu1325e@gmail.com>
2025-11-20 18:24:30 +0100

    Spring Boot init

---
+++ src/main/java/com/example/EnergyMix/EnergyMixApplication.java
@@ -0,0 +1,13 @@
+package com.example.EnergyMix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyMixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyMixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EnergyMix/EnergyMixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EnergyMix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyMixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
